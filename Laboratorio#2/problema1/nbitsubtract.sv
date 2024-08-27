7277fa66b8e4f2d1eb51355d66821828e7a96407
Kevrr <krr284@gmail.com>
2024-08-26 23:54:52 -0600

    fix to nbitsub

--- Laboratorio#2/problema1/nbitsubtract.sv
+++ Laboratorio#2/problema1/nbitsubtract.sv
@@ -1,8 +1,8 @@
 module nbitsubtract #(parameter n = 4)
 (input logic [n - 1: 0]a, b, output logic [n - 1: 0]y, output logic bout);
 	
-	logic [n:0] borrow;
-	logic [n - 1: 0] temp;
+	logic [n: 0] borrow;
+	logic [n - 1: 0] res, complement;
 	assign borrow[0] = 1'b0;
 	
 	genvar i;
@@ -12,15 +12,15 @@ module nbitsubtract #(parameter n = 4)
             .a(a[i]),
             .b(b[i]),
             .bin(borrow[i]),
-            .y(y[i]),
+            .y(res[i]),
             .bout(borrow[i + 1]));
     end
 	endgenerate
 	
 	assign bout = borrow[n];
 	
-	//if(bout) begin
-	//	assign temp = y;
-	//	negative neg(.a(temp), .y(y));
-	//end
+	negative neg(.a(y), .y(complement));
+	
+	assign y = bout ? complement : res;
+	
 endmodule
