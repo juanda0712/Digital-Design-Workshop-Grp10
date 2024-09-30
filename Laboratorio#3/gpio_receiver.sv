fd978687e669741b5769e6427a69436ca48fb509
Kevrr <krr284@gmail.com>
2024-09-30 01:27:03 -0600

    comm by SPI

--- Laboratorio#3/gpio_receiver.sv
+++
@@ -1,7 +0,0 @@
-module gpio_receiver(input clk, input clkPin, input [2:0] dataBus,
-							output reg [2:0] buttonStates);
-
-    always @(posedge clkPin) begin
-        buttonStates <= dataBus;
-    end
-endmodule
