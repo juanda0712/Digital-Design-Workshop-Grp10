73d48f1aca30386a52074bd0e271cb97b1770dd0
Kevrr <krr284@gmail.com>
2024-09-30 01:24:31 -0600

    comm by SPI

--- Laboratorio#3/p2inputs/p2inputs.ino
+++ Laboratorio#3/p2inputs/p2inputs.ino
@@ -1,24 +1,20 @@
+#include <SPI.h>
+
 const int button1Pin = 4;
 const int button2Pin = 5;
 const int button3Pin = 6;
 
-const int dataPin1 = 2;
-const int dataPin2 = 3;
-const int dataPin3 = 7;
-const int clkPin = 8;
+const int csPin = 10;
 
 void setup() {
   pinMode(button1Pin, INPUT_PULLUP);
   pinMode(button2Pin, INPUT_PULLUP);
   pinMode(button3Pin, INPUT_PULLUP);
+  
+  pinMode(csPin, OUTPUT);
+  digitalWrite(csPin, HIGH);
 
-  pinMode(dataPin1, OUTPUT);
-  pinMode(dataPin2, OUTPUT);
-  pinMode(dataPin3, OUTPUT);
-  pinMode(clkPin, OUTPUT);
-
-  digitalWrite(clkPin, LOW);
-
+  SPI.begin();
 }
 
 void loop() {
@@ -26,14 +22,14 @@ void loop() {
   int button2State = !digitalRead(button2Pin);
   int button3State = !digitalRead(button3Pin);
 
-  digitalWrite(dataPin1, button1State);
-  digitalWrite(dataPin2, button2State);
-  digitalWrite(dataPin3, button3State);
+  byte dataBus = (button1State << 2) | (button2State << 1) | button3State;
 
-  digitalWrite(clkPin, HIGH);
-  delayMicroseconds(1);  // Small delay for clock pulse
-  digitalWrite(clkPin, LOW);
+  SPI.beginTransaction(SPISettings(14000000, MSBFIRST, SPI_MODE0));
+  digitalWrite(csPin, LOW);
+  SPI.transfer(dataBus);
+  digitalWrite(csPin, HIGH);
+  SPI.endTransaction();
 
-  delay(1000);
+  delay(100);
 
 }
